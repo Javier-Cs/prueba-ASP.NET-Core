49da2ffd6993137b10c50e0057435af1d4a202f4
Javier-Cs <krchipullajavier@gmail.com>
2025-07-15 23:21:40 -0500

    Agregar archivos de proyecto.

---
+++ pruebaEntityFramework/Program.cs
@@ -0,0 +1,11 @@
+
+namespace pruebaEntityFramework
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("jsjj");
+        }
+    }
+}
