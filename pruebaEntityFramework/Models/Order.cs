e7a5bd02d697661058499f2c1ca7918a4ac6c71e
Javier-Cs <krchipullajavier@gmail.com>
2025-07-16 22:13:33 -0500

    implementación de la entidades como: - Order - User

---
+++ pruebaEntityFramework/Models/Order.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace pruebaEntityFramework.Models
+{
+    [Table("Orders", Schema ="store")]
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; } // propiedad de navegacion
+
+
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal TotalAmount { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Status { get; set; } = "enviado";
+
+        [Required]
+        [MaxLength(50)]
+        public string PaymentMethod { get; set; }
+
+        [Required]
+        [Column(TypeName ="decimal(10,2)")]
+        public decimal ShippingCost { get; set; }
+
+        // Propiedad de navegación para la relación con OrderItems (una orden puede tener muchos ítems)
+        public ICollection<OrderItem> OrderItems { get; set; }
+
+
+    }
+}
---
+++ pruebaEntityFramework/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace pruebaEntityFramework.Models
+{
+    [Table("Users", Schema = "store")]
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Phone { get; set; }
+
+        public DateTime RegisteredAt { get; set; } = DateTime.Now;
+
+        public bool IsActive { get; set; } = true;
+
+        // Propiedad de navegación para la relación con Orders (un usuario puede tener muchas órdenes)
+        public ICollection<Order> Orders { get; set; }
+    }
+}
