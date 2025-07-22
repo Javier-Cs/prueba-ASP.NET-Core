9ba28b4626592cc33b17e1efa4ba9fb39137485d
Javier-Cs <krchipullajavier@gmail.com>
2025-07-21 21:02:52 -0500

    implementacion de metodos Get, GetById y post

---
+++ pruebaEntityFramework/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pruebaEntityFramework.Data;
+using pruebaEntityFramework.Models;
+
+namespace pruebaEntityFramework.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts() {
+            return await context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Brand)
+                .ToListAsync();
+        }
+
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id) {
+            var product = await context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Brand)
+                .FirstOrDefaultAsync(p => p.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product) {
+            context.Products.Add(product);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.ProductId}, product);
+        }
+
+
+
+    }
+}
