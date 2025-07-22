62918e2cb71cace72ef63b7e8c6072bdb1f09078
Javier-Cs <krchipullajavier@gmail.com>
2025-07-21 19:13:37 -0500

    definimos una clase a la que heredamos DbContext - definimos un contructor  y implementamos las propiedades DbSet<>para cada entidad - instanciamos el metodo OnModelCreating

--- pruebaEntityFramework/Data/ApplicationDbContext.cs
+++ pruebaEntityFramework/Data/ApplicationDbContext.cs
@@ -1,8 +1,54 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using pruebaEntityFramework.Models;
 
 namespace pruebaEntityFramework.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // definir propiedades DBSet para cada entidad
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Inventory> inventories { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<OrderItem> ordersItem { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.inventory)
+                .WithOne(i => i.Product)
+                .HasForeignKey<Inventory>(i => i.ProductId);
+
+            modelBuilder.Entity<Category>()
+                .HasIndex(c => c.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Brand>()
+                .HasIndex(b => b.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Product>()
+                .HasIndex(p => p.SKU)
+                .IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected ApplicationDbContext()
+        {
+        }
     }
 }
