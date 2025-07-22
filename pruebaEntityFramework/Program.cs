f1dbdd1db00f46adb76bbd4b0de026e2c0906ee3
Javier-Cs <krchipullajavier@gmail.com>
2025-07-21 19:44:05 -0500

    En el archivo 'Program.cs' en la raíz de tu proyecto y modifica su contenido para incluir la configuración del DbContext:

--- pruebaEntityFramework/Program.cs
+++ pruebaEntityFramework/Program.cs
@@ -1,11 +1,39 @@
 
+using Microsoft.EntityFrameworkCore;
+using pruebaEntityFramework.Data;
+
 namespace pruebaEntityFramework
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("jsjj");
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment()) {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+
         }
     }
 }
